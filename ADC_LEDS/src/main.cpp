40fb32c86caae680638b2ce24ac96272dd43f672
Lis <Lukasz.Lis@turck.com>
2022-07-26 21:26:59 +0200

    something

--- ADC_LEDS/src/main.cpp
+++ ADC_LEDS/src/main.cpp
@@ -3,19 +3,29 @@
 int odczyt = 0;       //odczyt z portu analogowego
 Servo serwomechanizm;  //tworzymy obiekt typu serwomechanizm
 int pozycja = 0;      // pozycja serwomechnizmu
+int  pozycja_poprzednia = 0; //domyślna pozycja poprzednia na zero
 
 void setup() {
-  pinMode(8, OUTPUT);
+  pinMode(8, OUTPUT);  //konfiguracja pinów pod LED
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
+  serwomechanizm.attach(6);  //serwomechanizm podłączony pod pin 6 PWM
+  Serial.begin(9600);  //uruchomienie UART
   }
 
 void loop() {
-   odczyt = analogRead(A5);
-   odczyt = map(odczyt, 0, 1022, 1, 5);
+   odczyt = analogRead(A5);  // odczyt wartości z potencjometru/czujnika
+   //odczyt = map(odczyt, 0, 1020, 1, 5);   //mapowanie odczytu z potencjometru na podziałce 0-1020 na wartości 1-5
+   pozycja = map(odczyt, 0, 1020, 300, 2000);   //mapowanie odczytu z potencjometru na podziałce 0-1020 na wartości 450-2000 dla małego serwa
+   if (abs(pozycja - pozycja_poprzednia) > 100) {  //sprawdzenie, czy pozycja obecna jest większa o 5 od poprzedniej
+   serwomechanizm.writeMicroseconds(pozycja);   //wykonanie ruchu
+   delay(500);  //opóźnienie
+   pozycja_poprzednia = pozycja;  //podpisanie poprzedniej pozycji jako obecnej
+   }
 
+   /*
    if (odczyt <= 1)
    {
     digitalWrite(8, HIGH);
@@ -48,7 +58,7 @@ void loop() {
     digitalWrite(11, HIGH);
     digitalWrite(12, LOW);
    }
-else if (odczyt == 5)
+   else if (odczyt == 5)
    {
     digitalWrite(8, LOW);
     digitalWrite(9, LOW);
@@ -56,5 +66,5 @@ else if (odczyt == 5)
     digitalWrite(11, LOW);
     digitalWrite(12, HIGH);
    }
-   delay(100);
+   delay(100); */
 }
