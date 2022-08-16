0d7cf413069d014f4aac5b1985657da9ae898586
Lukasz <lis.lukasz92@gmail.com>
2022-08-16 21:25:11 +0200

    1st millis tryout

--- Serwo_Interface/src/main.cpp
+++ Serwo_Interface/src/main.cpp
@@ -8,16 +8,24 @@
 #define czerwony A0  //
 #define zielony A1  // deklaracja podpięcia diody RGB pod piny analogowe
 #define niebieski A2 //
-#define testLED 10
+#define testLED 13
+#define refButton 10
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7); //informacja o podłączeniu wyświetlacza, pod jakie piny
 Servo serwomechanizm;
 int pozycja = 1100;
 int zmianaPoz = 10;
 int pozMin = 450;
 int pozMax = 2000;
+int stanLed = HIGH;
 unsigned long aktualnyCzas = 0;
+unsigned long zapisanyCzas = 0;
+const int kolorCzerwony = 1;
+int kolorZielony = 2;
+int kolorNiebieski = 3;
+// int color = 0;
 
-void setup() {
+void setup()
+{
   digitalWrite(czerwony, HIGH);
   digitalWrite(zielony, HIGH);
   digitalWrite(niebieski,HIGH);
@@ -30,6 +38,8 @@ void setup() {
   pinMode(czerwony, OUTPUT);
   pinMode(zielony,OUTPUT);   // ustawienie pinów jako Output
   pinMode(niebieski,OUTPUT);
+  pinMode(refButton, INPUT_PULLUP);
+  pinMode(testLED, OUTPUT);
   lcd.begin(16, 2); //Deklaracja typu wyświetlacza
   lcd.setCursor(0, 0); //Ustawienie kursora
   lcd.print("Ekran powitalny"); //Wyświetlenie tekstu powitalnego
@@ -37,26 +47,51 @@ void setup() {
   lcd.print("Bazowac naped?"); //Wyświetlenie tekstu z zapytaniem o bazowanie, 10 znaków
 }
 
+
+void LEDblink(int color)
+{
+  switch (color)
+  {
+  case kolorCzerwony:
+    stanLed = !stanLed;
+    digitalWrite(czerwony, stanLed);
+    break;
+  
+  default:
+    break;
+  }
+}
+
 void LEDtest()
 {
-
-  digitalWrite(czerwony, LOW);  //
-  delay(1000);                  //
-  digitalWrite(czerwony, HIGH); //
-  digitalWrite(zielony, LOW);   //
-  delay(1000);                  //  Test diodek RGB
-  digitalWrite(zielony, HIGH);  //
-  digitalWrite(niebieski, LOW); //
-  delay(1000);                  //
-  digitalWrite(niebieski,HIGH); //
+  aktualnyCzas = millis();
+  if (aktualnyCzas - zapisanyCzas >= 1000UL)
+  {
+    zapisanyCzas = aktualnyCzas;
+    LEDblink(kolorCzerwony);
   }
+    
+  // digitalWrite(czerwony, LOW);  //
+  // delay(1000);                  //
+  // digitalWrite(czerwony, HIGH); //
+  // digitalWrite(zielony, LOW);   //
+  // delay(1000);                  //  Test diodek RGB
+  // digitalWrite(zielony, HIGH);  //
+  // digitalWrite(niebieski, LOW); //
+  // delay(1000);                  //
+  // digitalWrite(niebieski,HIGH); //
+}
 
 
-void loop() {
-  //LEDtest();
+void loop()
+{
+  LEDtest();
   if (digitalRead(przyciskOK) == LOW)
   {
     digitalWrite(testLED, HIGH); 
   }
-  
+  else
+  {
+    digitalWrite(testLED, LOW);
+  }
 }
