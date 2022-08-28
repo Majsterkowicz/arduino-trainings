61fe957cd422d83fe5c50a7ef24aa6e6ef511662
Lukasz <lis.lukasz92@gmail.com>
2022-08-28 21:33:36 +0200

    update
    Zakomentowano testy ledów,
    Dodano pierwszą wersję podprogramu bazowania oraz zapisywania pozycji.

--- Serwo_Interface/src/main.cpp
+++ Serwo_Interface/src/main.cpp
@@ -9,13 +9,15 @@
 #define zielony A1  // deklaracja podpięcia diody RGB pod piny analogowe
 #define niebieski A2 //
 #define testLED 13
-#define refButton 10
+#define przyciskRef 10
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7); //informacja o podłączeniu wyświetlacza, pod jakie piny
 Servo serwomechanizm;
-int pozycja = 1100;
+int pozycja = 1150;
 int zmianaPoz = 10;
 int pozMin = 450;
 int pozMax = 2000;
+int pozRefMin = 0;
+int pozRefMax = 0;
 int stanLed = HIGH;
 unsigned long aktualnyCzas = 0;
 unsigned long zapisanyCzas = 0;
@@ -23,6 +25,7 @@ const int kolorCzerwony = 1;
 const int kolorZielony = 2;
 const int kolorNiebieski = 3;
 int stanLED = HIGH;
+bool wybor = true;
 // int color = 0;
 
 void setup()
@@ -39,15 +42,45 @@ void setup()
   pinMode(czerwony, OUTPUT);
   pinMode(zielony,OUTPUT);   // ustawienie pinów jako Output
   pinMode(niebieski,OUTPUT);
-  pinMode(refButton, INPUT_PULLUP);
+  pinMode(przyciskRef, INPUT_PULLUP);
   pinMode(testLED, OUTPUT);
   lcd.begin(16, 2); //Deklaracja typu wyświetlacza
   lcd.setCursor(0, 0); //Ustawienie kursora
   lcd.print("Ekran powitalny"); //Wyświetlenie tekstu powitalnego
-  lcd.setCursor(0, 1); //Ustawienie kursora
-  lcd.print("Bazowac naped?"); //Wyświetlenie tekstu z zapytaniem o bazowanie, 10 znaków
-}
+  }
 
+int bazowanie ()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Trwa bazowanie");
+  lcd.setCursor(0, 1);
+  lcd.blink();
+  digitalWrite(czerwony, HIGH);
+  digitalWrite(zielony, HIGH);
+  digitalWrite(niebieski, HIGH);
+  digitalWrite(niebieski, LOW);
+  while (digitalRead(przyciskRef) == HIGH)
+  {
+    serwomechanizm.writeMicroseconds(pozycja);
+    pozycja = pozycja - zmianaPoz;
+    delay(200);
+    if (pozycja <= pozMin)
+    {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Blad bazowania");
+    }
+    
+  }
+  while (digitalRead(przyciskRef) == LOW)
+  {
+    serwomechanizm.writeMicroseconds(pozycja);
+    pozycja = pozycja + zmianaPoz;
+    delay(200);
+  }
+  return pozycja;
+}
 
 void LEDblink(int color)
 {
@@ -59,50 +92,70 @@ void LEDblink(int color)
     break;
   case kolorZielony:
     stanLed = !stanLed;
-    digitalWrite(czerwony, stanLed);
+    digitalWrite(zielony, stanLed);
     break;
   case kolorNiebieski:
     stanLed = !stanLed;
-    digitalWrite(czerwony, stanLed);
+    digitalWrite(niebieski, stanLed);
     break;
   default:
     break;
   }
 }
 
-void LEDtest()
-{
-  aktualnyCzas = millis();
-  if (aktualnyCzas - zapisanyCzas >= 1000UL)
-  {
-    zapisanyCzas = aktualnyCzas;
-    LEDblink(kolorCzerwony);
-    //stanLED = !stanLED;
-    //digitalWrite(czerwony, stanLED);
-  }
+// void LEDtest()
+// {
+//   aktualnyCzas = millis();
+//   if (aktualnyCzas - zapisanyCzas >= 1000UL)
+//   {
+//     zapisanyCzas = aktualnyCzas;
+//     stanLed = !stanLed;
+//     digitalWrite(zielony, stanLed);
+//     //LEDblink(kolorNiebieski);
+//     //stanLED = !stanLED;
+//     //digitalWrite(czerwony, stanLED);
+//   }
     
-  // digitalWrite(czerwony, LOW);  //
-  // delay(1000);                  //
-  // digitalWrite(czerwony, HIGH); //
-  // digitalWrite(zielony, LOW);   //
-  // delay(1000);                  //  Test diodek RGB
-  // digitalWrite(zielony, HIGH);  //
-  // digitalWrite(niebieski, LOW); //
-  // delay(1000);                  //
-  // digitalWrite(niebieski,HIGH); //
-}
+//   // digitalWrite(czerwony, LOW);  //
+//   // delay(1000);                  //
+//   // digitalWrite(czerwony, HIGH); //
+//   // digitalWrite(zielony, LOW);   //
+//   // delay(1000);                  //  Test diodek RGB
+//   // digitalWrite(zielony, HIGH);  //
+//   // digitalWrite(niebieski, LOW); //
+//   // delay(1000);                  //
+//   // digitalWrite(niebieski,HIGH); //
+// }
+
+
 
 
 void loop()
 {
-  LEDtest();
-  if (digitalRead(przyciskOK) == LOW)
+  //lcd.clear();
+  lcd.setCursor(0, 0); //Ustawienie kursora
+  lcd.print("Bazowac naped?"); //Wyświetlenie tekstu z zapytaniem o bazowanie, 10 znaków
+  lcd.setCursor(0, 1); //Ustawienie kursora
+  lcd.print("T/N: "); //pytanie T/N, 5 znaków
+  wybor = true;
+  lcd.setCursor(6, 1);
+  lcd.print("T");
+  if (digitalRead(przyciskDown) == LOW)
   {
-    digitalWrite(testLED, HIGH);
+    lcd.setCursor(6, 1);
+    lcd.print("N");
+    wybor = false;
   }
-  else
+  else if (digitalRead(przyciskUp) == LOW)
   {
-    digitalWrite(testLED, LOW);
+    lcd.setCursor(6, 1);
+    lcd.print("T");
+    wybor = true;
   }
-
+  if (wybor == true && digitalRead(przyciskOK) == LOW)
+  {
+    pozRefMin = bazowanie();
+    lcd.setCursor(0, 1);
+    lcd.print(pozRefMin);
+  }
 }
