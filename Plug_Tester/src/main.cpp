be4ccecb43f1df840e8b4259953a8c8153f9b84f
Lukasz <lis.lukasz92@gmail.com>
2022-12-02 22:44:25 +0100

    Update main.cpp
    Lot of changes. Indicator LED status added to use it in test confirmation. New LEDtest added to inform of test (wires connections) are passed. Flashes until one or more connections are crossed. Then immediately turns off.
    Program works fine. Some tunes to be done later. Works with 3 wire connectors.

--- Plug_Tester/src/main.cpp
+++ Plug_Tester/src/main.cpp
@@ -4,65 +4,128 @@
 #define IOpin0 10   //definicja pinów wejściowo-wyjściowych - podpięcie pod piny wtyczki
 #define IOpin1 11
 #define IOpin2 12
-// #define TestPin1 4
-// #define TestPin2 3
-// #define TestLed 5
 #define LED12 7     //definicja pinów pod diody sygnalizacyjne
 #define LED23 6
 #define LED31 5
+#define LEDTest 13
 
-int Con12 = 0;      //zmienne do przechowywania statusu sygnalu na pinach
-int Con23 = 0;
-int Con31 = 0;
+bool result;
+bool testResult;
+
+int LED12stat = LOW;
+int LED23stat = LOW;
+int LED31stat = LOW;
+int LEDTeststat = LOW;
 
 
 void setup() {
   pinMode(IOpin0, INPUT_PULLUP);      //wstępnie, wszystkie piny jako input
   pinMode(IOpin1, INPUT_PULLUP);
   pinMode(IOpin2, INPUT_PULLUP);
-  // pinMode(TestPin1, INPUT_PULLUP);
-  // pinMode(TestPin2, OUTPUT);
   pinMode(LED12, OUTPUT);             //setup LED-pinów jako wyjścia
   pinMode(LED23, OUTPUT);
   pinMode(LED31, OUTPUT);
-  //pinMode(TestLed, OUTPUT);
-  digitalWrite(LED12, LOW);           //domyślnie wyłączenie wszystkich diodek
-  digitalWrite(LED23, LOW);
-  digitalWrite(LED31, LOW);
-  //digitalWrite(TestLed, LOW);
-  //digitalWrite(TestPin2, LOW);
+  pinMode(LEDTest, OUTPUT);
+  digitalWrite(LED12, LED12stat);           //domyślnie wyłączenie wszystkich diodek
+  digitalWrite(LED23, LED23stat);
+  digitalWrite(LED31, LED31stat);
+  digitalWrite(LEDTest, LEDTeststat);
+}
+
+int test (int pin){
+  pinMode(IOpin0, INPUT_PULLUP);      //wstępnie, wszystkie piny jako input
+  pinMode(IOpin1, INPUT_PULLUP);
+  pinMode(IOpin2, INPUT_PULLUP);
+
+  switch (pin)
+  {
+    case 0:
+    pinMode(IOpin0, OUTPUT);            //ustawienie pierwszego pinu jako wyjście sygnału, pozostałe piny jako wejścia do nasłuchu sygnału
+    digitalWrite(IOpin0, LOW);         //ustawienie stanu niskiego na pinie 0
+
+    if (digitalRead(IOpin1) == LOW)
+      {
+        LED12stat = HIGH;
+      }
+      else
+      {
+        LED12stat = LOW;
+      }
+    if (digitalRead(IOpin2) == LOW)
+      {
+        LED31stat = HIGH;
+      }
+      else
+      {
+        LED31stat = LOW;
+      }
+    break;
+
+    case 1:
+    pinMode(IOpin1, OUTPUT);                //ustawienie drugiego pinu jako wyjście sygnału, pozostałe piny jako wejścia do nasłuchu sygnału
+    digitalWrite(IOpin1, LOW);              //ustawienie stanu niskiego na pinie 1
+
+    if (digitalRead(IOpin0) == LOW)
+      {
+        LED12stat = HIGH;
+      }
+      else
+      {
+        LED12stat = LOW;
+      }
+    if (digitalRead(IOpin2) == LOW)
+    {
+      LED23stat = HIGH;
+    }
+      else
+      {
+        LED23stat = LOW;
+      }
+    break;
+    
+    case 2:
+    pinMode(IOpin2, OUTPUT);                //ustawienie trzeciego pinu jako wyjście sygnału, pozostałe piny jako wejścia do nasłuchu sygnału
+    digitalWrite(IOpin2, LOW);              //ustawienie stanu niskiego na pinie 2
+
+    if (digitalRead(IOpin0) == LOW)
+      {
+        LED31stat = HIGH;
+      }
+      else
+      {
+        LED31stat = LOW;
+      }
+    if (digitalRead(IOpin1) == LOW)
+      {
+        LED23stat = HIGH;
+      }
+      else
+      {
+        LED23stat = LOW;
+      }
+    break;
+  }
+  digitalWrite(LED12, LED12stat);
+  digitalWrite(LED23, LED23stat);
+  digitalWrite(LED31, LED31stat);
+    if (LED12stat == HIGH || LED23stat == HIGH || LED31stat == HIGH)
+      {
+        digitalWrite(LEDTest, LOW);
+      }
+      else
+      {
+        digitalWrite(LEDTest, HIGH);
+      }
+  return result;
 }
 
 void loop() {
-  pinMode(IOpin0, OUTPUT);            //ustawienie pierwszego pinu jako wyjście sygnału
-  pinMode(IOpin1, INPUT_PULLUP);      //ustawienie pozostałych pinów jako wejścia do nasłuchu sygnału
-  pinMode(IOpin2, INPUT_PULLUP);
-  digitalWrite(IOpin0, LOW);         //ustawienie stanu wysokiego na pinie 0
-  
-  if (digitalRead(IOpin1) == LOW)
+  for (size_t i = 0; i < 3; i++)
   {
-    digitalWrite(LED12, HIGH);
-    delay(500);
+    testResult = test(i);
+    if (i == 2)
+    {
+      i = 0;
+    }
   }
-  else
-  {
-    digitalWrite(LED12, LOW);
-  }
-  if (digitalRead(IOpin2) == LOW)
-  {
-    digitalWrite(LED31, HIGH);
-    delay(500);
-  }
-  else
-  {
-    digitalWrite(LED31, LOW);
-  }
-
-  // if (digitalRead(TestPin1) == LOW)
-  // {
-  //   digitalWrite(TestLed, HIGH);
-  //   delay(1000);
-  // }
-  //   digitalWrite(TestLed, LOW);
-  
 }
